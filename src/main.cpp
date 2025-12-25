48b678439d073ad3b2de8de5d5d402c2fd9ead83
Vlad <rvy@ukr.net>
2025-12-25 19:37:24 +0200

    save

--- src/main.cpp
+++ src/main.cpp
@@ -337,7 +337,16 @@ int main(int argc, char *argv[]) {
     });
 
 
-    static_mgr();// TODO - Посмотреть как реализовано в старых рабочих коммитах
+
+    static_box_manager static_mgr({
+                                          STATIC_AUTO_REBIND_ON_LOSS,
+                                          STATIC_REBIND_TIMEOUT_MS,
+                                          STATIC_PARENT_IOU_TH,
+                                          STATIC_REATTACH_SCORE_TH
+                                  });
+
+
+//    static_mgr();// TODO - Посмотреть как реализовано в старых рабочих коммитах
 
     std::vector<cv::Rect2f> dynamic_boxes;
     std::vector<int> dynamic_ids;
